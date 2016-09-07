bad8ddc34ed1251d1ef5d169c62e6c7dfc454a0b
RANCiD <S614479adm>
2016-09-07 03:07:29 +0200

    Upgrade from version 3.4.0 to 3.5.1

--- include/config.h
+++ include/config.h
@@ -25,13 +25,13 @@
 #define HAVE_INTTYPES_H 1
 
 /* Define to 1 if you have the <libutil.h> header file. */
-/* #undef HAVE_LIBUTIL_H */
+#define HAVE_LIBUTIL_H 1
 
 /* Define to 1 if you have the <limits.h> header file. */
 #define HAVE_LIMITS_H 1
 
 /* Define to 1 if you have the <malloc.h> header file. */
-#define HAVE_MALLOC_H 1
+/* #undef HAVE_MALLOC_H */
 
 /* Define to 1 if you have the `memcpy' function. */
 #define HAVE_MEMCPY 1
@@ -58,13 +58,16 @@
 /* #undef HAVE_PTSNAME */
 
 /* Define to 1 if you have the <pty.h> header file. */
-#define HAVE_PTY_H 1
+/* #undef HAVE_PTY_H */
 
 /* Define to 1 if you have the `rindex' function. */
 #define HAVE_RINDEX 1
 
 /* Define to 1 if you have the <siginfo.h> header file. */
 /* #undef HAVE_SIGINFO_H */
+
+/* Define to 1 if you have the <stdarg.h> header file. */
+#define HAVE_STDARG_H 1
 
 /* Define to 1 if you have the <stdint.h> header file. */
 #define HAVE_STDINT_H 1
@@ -111,6 +114,9 @@
 /* Define to 1 if you have the <sys/stat.h> header file. */
 #define HAVE_SYS_STAT_H 1
 
+/* Define to 1 if you have the <sys/time.h> header file. */
+#define HAVE_SYS_TIME_H 1
+
 /* Define to 1 if you have the <sys/types.h> header file. */
 #define HAVE_SYS_TYPES_H 1
 
@@ -127,7 +133,7 @@
 /* #undef HAVE_UTIL_H */
 
 /* Define to 1 if you have the <wait.h> header file. */
-#define HAVE_WAIT_H 1
+/* #undef HAVE_WAIT_H */
 
 /* Name of package */
 #define PACKAGE "rancid"
@@ -139,7 +145,7 @@
 #define PACKAGE_NAME "rancid"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "rancid 3.4.1"
+#define PACKAGE_STRING "rancid 3.5.1"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "rancid"
@@ -148,7 +154,7 @@
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "3.4.1"
+#define PACKAGE_VERSION "3.5.1"
 
 /* Define as the return type of signal handlers (`int' or `void'). */
 #define RETSIGTYPE void
@@ -157,7 +163,7 @@
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#define VERSION "3.4.1"
+#define VERSION "3.5.1"
 
 /* Define to empty if `const' does not conform to ANSI C. */
 /* #undef const */
